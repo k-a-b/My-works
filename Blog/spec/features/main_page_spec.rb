7b9cb7563f060acc071f1c64a4c776047f8cdd5b
k_a_b <k_a_b@bk.ru>
2019-05-12 00:46:10 +0300

    Add authenticate

--- Blog/spec/features/main_page_spec.rb
+++ Blog/spec/features/main_page_spec.rb
@@ -16,17 +16,12 @@ feature 'Visit main page' do
     context 'user aka moderator' do
 
       scenario 'want create new user' do
-        sleep 2
         click_link 'Users'
-        sleep 2
         click_link 'New User'
-        sleep 2
         fill_in 'Name', with: 'User_test_1'
-        sleep 2
         fill_in 'Email', with: 'User_test_1@test.ru'
-        sleep 2
         click_on 'Create User'
-# sleep
+
         expect(page).to have_text('User was successfully created.')
       end
     end
