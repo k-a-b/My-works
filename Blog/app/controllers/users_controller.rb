12dfa9b937c2774a43ac21bbe38d89f3be4c6892
k_a_b <k_a_b@bk.ru>
2019-05-12 14:56:05 +0300

    Simple authorization

--- Blog/app/controllers/users_controller.rb
+++ Blog/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, except: [:index, :show, :new]
 
   # GET /users
   # GET /users.json
