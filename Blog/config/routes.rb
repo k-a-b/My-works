7eb910b0de256efb4ebdd6b6e9ef4279416d06b3
k_a_b <k_a_b@bk.ru>
2019-05-18 18:00:22 +0300

    Add gem bootstrap and configure main page under it

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   resources :users
   resources :messages
   resources :sessions
-  root 'users#new'
+  root 'main#index'
   get 'log_in', to: 'sessions#new'
   get 'log_out', to: 'sessions#destroy'
   get 'sing_up', to: 'users#new'
   get 'main', to: 'main#index'
   get 'messages/page/(:page(.:format))', to: 'messages#index'
-  get 'messages', to: 'messages#index'
-  get 'users', to: 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
