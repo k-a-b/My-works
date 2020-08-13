295db0a5dccbf5dbc29da71d7e9cfbcf1f0f9689
k_a_b <arseny.karashkevich@balance-pl.ru>
2020-08-13 11:53:39 +0300

    remove blog

--- Blog/app/models/message.rb
+++
@@ -1,6 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :user
-  has_many :comments, as: :commentable, dependent: :destroy
-  has_many :users, through: :comments
-  has_many :comentators, through: :comments, source: :user
-end
--- Blog/app/views/messages/_message.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! message, :id, :title, :content, :user_id, :visible, :created_at, :updated_at
-json.url message_url(message, format: :json)
